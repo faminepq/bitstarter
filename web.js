5b93278ad12a38e96a8909a50d961da5b35d76d4
Ubuntu <ubuntu@ip-172-31-45-217.us-west-2.compute.internal>
2013-07-09 22:37:50 +0000

    fixed index.html processing

--- web.js
+++ web.js
@@ -1,13 +1,19 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+var app = express.createServer(express.logger());
+var port = process.env.PORT || 5000;
+var fs = require('fs');
 
 fs.readFile('./index.html', function (err, html) {
     if (err) {
-        throw err; 
+        throw err;
     }
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+
+app.get('/', function(request, response) {
+  response.send(html.toString());
+});
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 });
